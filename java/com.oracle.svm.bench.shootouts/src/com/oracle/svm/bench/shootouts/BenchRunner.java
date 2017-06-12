231f2cbf20483c505c6ae7f88604cac8cea62bbe
Aleksandar Pejović <pejovica@users.noreply.github.com>
2017-06-12 12:57:18 +0200

    Replace System.out with an in-memory stream

--- java/com.oracle.svm.bench.shootouts/src/com/oracle/svm/bench/shootouts/BenchRunner.java
+++ java/com.oracle.svm.bench.shootouts/src/com/oracle/svm/bench/shootouts/BenchRunner.java
@@ -1,7 +1,6 @@
 package com.oracle.svm.bench.shootouts;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.function.Consumer;
@@ -9,45 +8,43 @@ import java.util.function.Consumer;
 class BenchRunner {
 
     static void run(Consumer<String[]> bench, String benchName, String[] args) {
+        Integer nRuns = Integer.valueOf(args[1]);
         String[] benchArgs = Arrays.copyOfRange(args, 2, args.length);
-        bench.accept(benchArgs);
 
-        System.out.println("---- BEGIN WARM UP ----");
-        System.out.println("Performing " + args[0] + " rounds.");
-        System.out.println();
+        PrintStream systemOut = System.out;
 
-        // ignore output
-        PrintStream out = System.out;
-        System.setOut(new PrintStream(new OutputStream() {
-            @Override
-            public void write(int b) throws IOException {
+        // let benches write to memory
+        ByteArrayOutputStream benchOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(benchOut));
 
-            }
-        })
-                        );
+        systemOut.println("---- BEGIN WARM UP ----");
+        systemOut.println("Performing " + args[0] + " rounds.");
+        systemOut.println();
         for (int i = 0; i < Integer.valueOf(args[0]); i++) {
             bench.accept(benchArgs);
+            benchOut.reset();
         }
-        // restore output
-        System.setOut(out);
-        System.out.println("---- END WARM UP ----");
+        systemOut.println("---- END WARM UP ----");
 
 // System.gc();
 
-        System.out.println("---- BEGIN MEASURING PERFORMANCE ----");
-        System.out.println("Performing " + args[1] + " runs.");
-        System.out.println();
-
-        Integer nRuns = Integer.valueOf(args[1]);
+        systemOut.println("---- BEGIN MEASURING PERFORMANCE ----");
+        systemOut.println("Performing " + args[1] + " runs.");
+        systemOut.println();
         long start = System.nanoTime();
         for (int i = 0; i < nRuns; i++) {
+            benchOut.reset();   // reset before a run, so that we get to keep the result of the last run
             bench.accept(benchArgs);
         }
         long stop = System.nanoTime();
-        System.out.println("---- END MEASURING PERFORMANCE ----");
+        systemOut.println("---- END MEASURING PERFORMANCE ----");
 
-        System.out.println();
-        System.out.println("*** " + benchName.toUpperCase() + " RESULTS: Average time based on " + args[1] + " runs is " + String.valueOf((stop - start) / nRuns * 10e-3) + " us ***");
-        System.out.println();
+        systemOut.println("---- BEGIN OUTPUT ----");
+        systemOut.print(benchOut.toString());
+        systemOut.println("---- END OUTPUT ----");
+
+        systemOut.println();
+        systemOut.println("*** " + benchName.toUpperCase() + " RESULTS: Average time based on " + args[1] + " runs is " + String.valueOf((stop - start) / nRuns * 10e-3) + " us ***");
+        systemOut.println();
     }
 }
