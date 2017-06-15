3f261fdb0d13ffbea3a4804ecb684cbfc221b79f
Aleksandar Pejović <pejovica@users.noreply.github.com>
2017-06-15 14:00:16 +0200

    Add multi-project support

--- mx.aot-bench/mx_aot_bench.py
+++ mx.aot-bench/mx_aot_bench.py
@@ -5,10 +5,7 @@ _suite = mx.suite('aot-bench')
 jdk = mx.get_jdk()
 jdk.aot_image = mx.exe_suffix(mx.join(jdk.home, 'bin', 'aot-image'))
 
-_project_name = 'com.oracle.svm.bench.shootouts'
-# _project_name = 'com.oracle.svm.bench.scalariform'
-
-_benchmarks = {
+_bench_suite = {
     'fannkuchredux': [('15000', '10', '9'), ['-H:+MultiThreaded']],
     'mandelbrot': [('15000', '10', '500'), ['-H:+MultiThreaded']],
     'binarytrees': [('15000', '10', '14'), ['-H:+MultiThreaded']],
@@ -25,60 +22,77 @@ _benchmarks = {
 }
 
 
-def _get_project(name):
-    for p in _suite.projects:
-        if p.name == name:
-            return p
-    raise ValueError("'{}' is not aot-bench suite's project.".format(name))
+class Bench(object):
+    def __init__(self, src_file, class_file, classpath, runtime_args, build_args):
+        self.src_file = src_file
+        self.class_file = class_file
+        self.classpath = classpath
+        self.runtime_args = runtime_args
+        self.build_args = build_args
 
 
-def _build_aot_images(project, benchmarks):
+def _collect_bench_data(bench_suite):
+    benchmarks = {}
+    for bench in bench_suite:
+        for project in _suite.projects:
+            try:
+                bench_class, (bench_src, _) = project.find_classes_with_annotations(None, [
+                    '@AOTBench("{}")'.format(bench)]).popitem()
+            except KeyError:
+                continue
+            benchmarks[bench] = Bench(bench_src, bench_class, mx.classpath(names=project, jdk=jdk), *bench_suite[bench])
+            break
+        else:
+            raise ValueError("Could not find a '{}' benchmark. Check the name in java sources.".format(bench))
+    return benchmarks
+
+
+def _build_aot_images(benchmarks):
     print('---------------- BEGIN BUILDING AOT IMAGES ----------------')
-    output_dir = project.output_dir()
-    class_files = {}
-    for bench in benchmarks:
-        bench_class, (bench_src, _) = project.find_classes_with_annotations(None, [
-            '@AOTBench("{}")'.format(bench)]).popitem()
-        class_files[bench] = bench_class
-        witness = mx.TimeStampFile(mx.join(output_dir, bench))
-        if not witness.exists() or witness.isOlderThan(bench_src):
-            cmd = [jdk.aot_image, '-cp', mx.classpath(names=project, jdk=jdk), '-H:Name={}'.format(bench),
-                   '-H:Class={}'.format(bench_class)]
-            cmd.extend(benchmarks[bench][1])
+    output_dir = mx.join(_suite.get_output_root(), 'bin')
+    mx.ensure_dir_exists(output_dir)
+    for bench_name, bench in benchmarks.items():
+        witness = mx.TimeStampFile(mx.join(output_dir, bench_name))
+        if not witness.exists() or witness.isOlderThan(bench.src_file):
+            cmd = [jdk.aot_image, '-cp', bench.classpath, '-H:Name={}'.format(bench_name),
+                   '-H:Class={}'.format(bench.class_file)]
+            cmd.extend(bench.build_args)
             print(' '.join(cmd))
             mx.run(cmd, cwd=output_dir)
     print('---------------- END BUILDING AOT IMAGES ----------------')
-    return class_files
+
+
+def _run(benchmarks):
+    print('---------------- BEGIN RUNNING AOT BENCHMARKS ----------------')
+    for bench_name, bench in benchmarks.items():
+        common_args = ['-XX:+PrintGC']
+        print('------------ BEGIN {} BENCHMARK ------------'.format(bench_name.upper()))
+        print('-------- BEGIN GRAAL RUN --------')
+        graal_cmd = [jdk.java, '-cp', bench.classpath] + common_args
+        graal_cmd.append(bench.class_file)
+        graal_cmd.extend(bench.runtime_args)
+        print(' '.join(graal_cmd))
+        mx.run(graal_cmd, cwd=_suite.get_output_root())
+        print('-------- END GRAAL RUN --------')
+        print('-------- BEGIN SVM RUN --------')
+        svm_cmd = [mx.join('bin', bench_name)] + common_args
+        svm_cmd.append('10')  # set fixed number of warm up rounds
+        svm_cmd.extend(bench.runtime_args[1:])
+        print(' '.join(svm_cmd))
+        mx.run(svm_cmd, cwd=_suite.get_output_root())
+        print('-------- END SVM RUN --------')
+        print('------------ END {} BENCHMARK ------------'.format(bench_name.upper()))
+    print('---------------- END RUNNING AOT BENCHMARKS ----------------')
 
 
 def aot_benchmark(args):
     for bench in args:
-        if bench not in _benchmarks:
+        if bench not in _bench_suite:
             raise ValueError("'{}' is not an AOT benchmark.".format(bench))
 
-    project = _get_project(_project_name)
-    benchmarks = {bench: _benchmarks[bench] for bench in args} if args else _benchmarks
-    class_files = _build_aot_images(project, benchmarks)
-    print('---------------- BEGIN RUNNING AOT BENCHMARKS ----------------')
-    for bench in benchmarks:
-        common_args = ['-XX:+PrintGC']
-        print('------------ BEGIN {} BENCHMARK ------------'.format(bench.upper()))
-        print('-------- BEGIN GRAAL RUN --------')
-        graal_cmd = [jdk.java, '-cp', mx.classpath(names=project, jdk=jdk)] + common_args
-        graal_cmd.append(class_files[bench])
-        graal_cmd.extend(benchmarks[bench][0])
-        print(' '.join(graal_cmd))
-        mx.run(graal_cmd, cwd=project.output_dir())
-        print('-------- END GRAAL RUN --------')
-        print('-------- BEGIN SVM RUN --------')
-        svm_cmd = [mx.join('.', bench)] + common_args
-        svm_cmd.append('10')  # set fixed number of warm up rounds
-        svm_cmd.extend(benchmarks[bench][0][1:])
-        print(' '.join(svm_cmd))
-        mx.run(svm_cmd, cwd=project.output_dir())
-        print('-------- END SVM RUN --------')
-        print('------------ END {} BENCHMARK ------------'.format(bench.upper()))
-    print('---------------- END RUNNING AOT BENCHMARKS ----------------')
+    benchmarks = _collect_bench_data({bench: _bench_suite[bench] for bench in args} if args else _bench_suite)
+    _build_aot_images(benchmarks)
+    _run(benchmarks)
 
 
 mx.update_commands(_suite, {
